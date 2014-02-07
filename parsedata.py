7532a0c1065ccc7bb117d9dc9425dd76fc53a0de
Kaitlin <kayak0806@yahoo.com>
2014-02-07 14:47:20 -0500

    fixt parsedata for input

--- parsedata.py
+++ parsedata.py
@@ -5,18 +5,45 @@ Each datapoint is an array of datetime, light, depth, temeperature."""
 
 
 import datetime
+import argparse
 
-def main():
-	f = open('drifter.tab')
-	f.readline()
+def openFile(fname):
+  
+
+  fin = open(fname)
+
+  return fin
+
+def parseFile(fname):
+	fin = openFile(fname)
+	print 'merp'
+	fin.readline()
 	points = []
-	for line in f:
+	for line in fin:
+	  #line: year, month, day, hour, minute, second, light, depth, temp
 		line = line.split(" ")
 		line = [int(i) for i in line]
 		date = datetime.datetime(line[0],line[1],line[2],line[3],line[4],line[5])
 		point = [date,line[6],line[7],line[8]]
 		points.append(point)
 	return points
+	
+
+def main():
+
+  parser = argparse.ArgumentParser()
+  parser.add_argument('name')
+  args = parser.parse_args()
+
+  fname = args.name
+  print parseFile(fname)
+  
+  
+  #print parseFile('drifter.tab')
 
 if __name__ == "__main__":
 	main()
+	
+	
+	
+	
