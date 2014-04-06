6d6a9283b838a4638970ffcd5c64c59403c82d9e
jchen <jchen@ubuntu.(none)>
2014-04-06 02:46:43 -0400

    added confidence intervals

--- simulateMain.py
+++ simulateMain.py
@@ -3,6 +3,8 @@ import sunloc
 import numpy
 import thinkplot
 import introduceError
+import matplotlib.pyplot as plt
+import scipy.stats
 # import (error stuff)
 '''
 plan for main:
@@ -15,7 +17,7 @@ plan for main:
 '''
 # general parameters:
 startDate = datetime.date(2014,1,1) # jan 01, 2014
-endDate = datetime.date(2014,12,31) # dec 31, 2014
+endDate = datetime.date(2014,1,31) # dec 31, 2014
 lat,lon = (42.291625,-71.264406)    # Olin College
 
 # generate days
@@ -61,6 +63,12 @@ locations = []
 for day in errorsLat:
     locations.append(allLocations(day))
 
+def mean_confidence_interval(data, confidence=0.95):
+    a = 1.0*numpy.array(data)
+    n = len(a)
+    m, se = numpy.mean(a), scipy.stats.sem(a)
+    h = se * scipy.stats.t._ppf((1+confidence)/2., n-1)
+    return m, m-h, m+h
 
 # find accuracy
 print 'accuracy'
@@ -69,16 +77,36 @@ def accuracy(locations, start):
     lats,lons = zip(*locations)
     dlat = [l-lat for l in lats]
     dlon = [l-lon for l in lons]
-    meanLat = numpy.mean(dlat)
-    meanLon = numpy.mean(dlon)
-    return (meanLat, meanLon)
-averages = []
+    latinfo = mean_confidence_interval(dlat)
+    loninfo = mean_confidence_interval(dlon)
+
+    #meanLat = numpy.mean(dlat)
+    #meanLon = numpy.mean(dlon)
+    #return (meanLat, meanLon)
+    return latinfo,loninfo
+latinfos = []
+loninfos= []
 for day in locations:
-    averages.append(accuracy(day,(lat,lon)))
+    latinfo,loninfo=accuracy(day,(lat,lon))
+    latinfos.append(latinfo)
+    loninfos.append(loninfo)
+
+print 'averages'
+
+#means,uppers,lowers = zip(*latinfos)
+means,uppers,lowers = zip(*loninfos)
 
 # plot accuracy
-print len(averages), len(dates)
-aveLat,aveLon = zip(*averages)
-thinkplot.Scatter(dates,aveLat)
-# thinkplot.Scatter(dates,aveLon)
-thinkplot.Show()
+
+#print len(averages), len(dates)
+#aveLat,aveLon = zip(*averages)
+
+
+#thinkplot.Scatter(dates,aveLat)
+
+plt.plot(dates,means)
+plt.plot(dates,uppers)
+plt.plot(dates,lowers)
+plt.show()
+#thinkplot.Scatter(dates,aveLon)
+#thinkplot.Show()
