62f8d69eb0ffd7ce33458bf883ce1ee02f05db2e
Kaitlin Gallagher <kkgallagher9@gmail.com>
2014-04-22 14:34:30 -0400

    working on sunloc2

--- sunloc.py
+++ sunloc.py
@@ -2,6 +2,8 @@ import Pysolar
 from Pysolar import util
 import datetime
 import math
+import numpy
+from matplotlib import pyplot as plt
 
 '''calcLatLon gets rise/set from lat/lon
 	calcRiseSet gets lat/lon from rise/set'''
@@ -34,10 +36,9 @@ def calcLatLon(riseDate,setDate, approx=(30,-60)):
 			dist = ( altRise**2 + altSet**2 )**0.5
 			nextVal.append((dist,(lat,lon)))
 		current = min(nextVal)
-	print i
 	return current[1]
 
-def calcLatLon2(riseDate,setDate, approx=(42,-71)):
+def calcLatLon2(riseDate,setDate, approx=(20,-50)):
 	# NOT WORKING YET
 	# riseDate: datetime when rising sun is at horizon
 	# setDate: datetime when setting sun is at horizon
@@ -45,31 +46,45 @@ def calcLatLon2(riseDate,setDate, approx=(42,-71)):
 	# returns: matchimg position
 	date = riseDate.date()
 
-	riseApprox,setApprox = calcRiseSet(approx[0],approx[1],date)
-	dist = ( (riseDate-riseApprox).total_seconds()**2 + (setDate-setApprox).total_seconds()**2 )**0.5
-	current = (dist,approx)
-	next=[]
-
-	# while (current[0] >= 0):
-	for i in range(10):
-		print current[0]
-		latCur,lonCur = current[1]
+	def search(val):
+		dist, guess,path = val
+		print dist
+		if dist<10:
+			print dist
+			return guess
+		
+		glat,glon = guess
 		next = [
-			(latCur+10,lonCur),
-			(latCur,lonCur+10),
-			(latCur-10,lonCur),
-			(latCur,lonCur-10)
+			(glat-.1,glon),
+			(glat,glon-.5),
+			(glat+.1,glon),
+			(glat,glon+.5)
 			]
-		nextVal = []
-		for lat,lon in next:
-			print lat,lon
-			riseCur,setCur = calcRiseSet(lat,lat,date)
-			risedist = (riseDate-riseCur).total_seconds()
-			setdist = (setDate-setCur).total_seconds()
-			dist = ( risedist**2 + setdist**2 )**0.5
-			nextVal.append( (dist,(lat,lon)) )
-		current = min(nextVal)
-	return current[1]
+		vals = []
+		
+		for n in next:
+			if n not in path:
+				nrise,nset = calcRiseSet(n[0],n[1],date)
+				risedist = (riseDate-nrise).total_seconds()
+				setdist = (setDate-nset).total_seconds()
+				newdist = ( (risedist)**2 + (setdist)**2)**.5
+				newpath = path+[n]
+				vals.append((newdist,n,newpath))
+		if not vals:
+			# for p in path[100::]:
+				# print p
+			return guess
+		return search(min(vals))
+
+	grise,gset = calcRiseSet(approx[0],approx[1],date)
+	risedist = (riseDate-grise).total_seconds()
+	setdist = (setDate-gset).total_seconds()
+	dist = ( (risedist)**2 + (setdist)**2)**.5			
+
+	return search( (dist,approx,[]) )
+
+
+
 
 def calcRiseSet(lat,lon,date):
 	# takes a lat, lon, and datetime.date object
@@ -99,9 +114,9 @@ def main():
 		def __init__(self,scale):
 			self.scale = scale
 			self.values = dict()
-			maxlat,maxlon = 90/scale,180/scale
-			self.lats = [i-(90+1)  for i in range(90*2+1)]
-			self.lons = [i-(180+1) for i in range(180*2+1)]
+			latRange,lonRange = (-90,90),(-180,180)
+			self.lats = [latRange[0]+scale*i  for i in range(int((latRange[1]-latRange[0])/scale))]
+			self.lons = [lonRange[0]+scale*i  for i in range(int((lonRange[1]-lonRange[0])/scale))]
 			for lat in self.lats:
 				for lon in self.lons:
 					self.values[(lat,lon)]=0
@@ -122,21 +137,43 @@ def main():
 			else:
 				print "not a valid location\n"
 				return 0
+		def toArray(self):
+			a = len(self.lats)
+			b = len(self.lons)
+			matrix = [[i for i in range(b)] for j in range(a)]
+			for x in range(a):
+				for y in range(b):
+					lat,lon = self.lats[x],self.lons[y]
+					val = self.value((lat,lon))
+					matrix[x][y] = val
+			return numpy.array(matrix)
 
-	world = World(10)
+	world = World(1)
 	today = datetime.date.today()
-	riseTime = datetime.time(10,2)
-	setTime = datetime.time(23,26)
-	riseDate = datetime.datetime.combine(today,riseTime)
-	setDate = datetime.datetime.combine(today,setTime)
+	# riseTime = datetime.time(10,2)
+	# setTime = datetime.time(23,26)
+	# riseDate = datetime.datetime.combine(today,riseTime)
+	# setDate = datetime.datetime.combine(today,setTime)
+	lat,lon = 42,-71.264406
+	riseDate,setDate = calcRiseSet(0,0,today)
 
 	for pos in world.allPos():
 		lat,lon = pos
+		# Calculate 'cost'
+		# riseCur,setCur = calcRiseSet(lat,lon,today)
+		# risedist = (riseDate-riseCur).total_seconds()
+		# setdist = (setDate-setCur).total_seconds()
+		# dist = ( (risedist)**2 + (setdist)**2)**.5
 		altRise = Pysolar.GetAltitude(lat,lon, riseDate)
 		altSet = Pysolar.GetAltitude(lat,lon, setDate)
 		dist = ( altRise**2 + altSet**2 )**0.5
+		if dist==0:
+			print pos, dist
 		world.write(pos,dist)
 	print len(world.allPos())
+
+	plt.pcolor(world.toArray())	
+	plt.show()
 		
 
 
